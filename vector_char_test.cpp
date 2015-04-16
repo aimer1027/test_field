b89783af6c7ea0af32b310d6ca7d06fa5e767b0c
root <root@anzhy.centOS>
2015-04-16 21:56:47 +0800

    test whether vector<char> element usable without push_back answer is no

---
+++ vector_char_test.cpp
@@ -0,0 +1,18 @@
+#include <cstdio>
+#include <vector>
+
+using namespace std ;
+
+/*
+  this is used to test wherether we could use 
+  the char element in the vector with out push_back first
+*/
+
+int main ()
+{
+ vector<char> char_list ;
+
+ printf ("%c" , char_list[0]) ;
+
+ return 0 ;
+}
